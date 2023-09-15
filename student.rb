0f7f49a9c5a377edf9308d3fb81de7b7283649f4
Briton Otieno <ojwangbriton@gmail.com>
2023-09-15 21:12:01 +0300

    Update student file

--- student.rb
+++ student.rb
@@ -5,7 +5,13 @@ class Student < Person
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+    @classroom = nil
+    self.classroom = classroom if classroom
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
