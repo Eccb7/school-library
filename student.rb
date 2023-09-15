5276d2b575a247b10b0cd62508f1f06a317c718c
Briton Otieno <ojwangbriton@gmail.com>
2023-09-15 21:17:39 +0300

    Update student file

--- student.rb
+++ student.rb
@@ -9,12 +9,12 @@ class Student < Person
     self.classroom = classroom if classroom
   end
 
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
-  end
-
-  def play_hooky
-    '¯\(ツ)/¯'
   end
 end
