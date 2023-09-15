c29e18b8a85039ad8ec6518b77b4396a344fb571
Briton Otieno <ojwangbriton@gmail.com>
2023-09-15 18:23:00 +0300

    Add book class file

---
+++ book.rb
@@ -0,0 +1,17 @@
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    rental = Rental.new(self, person, date)
+    @rentals << rental
+    person.rentals << rental
+    rental
+  end
+end
