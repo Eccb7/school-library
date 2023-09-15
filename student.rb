878d0ee64909f568582a9fc48d938107de0d9849
Briton Otieno <ojwangbriton@gmail.com>
2023-09-15 21:57:16 +0300

    Update student file

--- student.rb
+++ student.rb
@@ -3,13 +3,17 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = nil
-    self.classroom = classroom if classroom
+    @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def join_classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
