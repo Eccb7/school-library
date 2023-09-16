4d7c0078bfa792d00b9f5131178c773db5c79354
Briton Otieno <ojwangbriton@gmail.com>
2023-09-17 00:11:17 +0300

    Correct rubocop errors

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :rentals
-  
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
