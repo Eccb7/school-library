1d60d5185a628a51ed561381d3152cb919c6a311
Briton Otieno <ojwangbriton@gmail.com>
2023-09-16 21:37:12 +0300

    Correct linters

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, name = 'Unknown', specialization)
-    super(age, name: name, parent_permission: true)
+  def initialize(age, specialization, name = 'Unknown')
+    super(age, true, name)
     @specialization = specialization
   end
 
